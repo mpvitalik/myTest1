a8ac32ee91d626e9674c5c9e966aa96e8c78ed22
vitalii <mpvitalik@gmail.com>
2025-03-28 15:55:13 +0200

    add Gifts

--- src/test/java/alltests/Cashier.java
+++ src/test/java/alltests/Cashier.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -36,6 +37,7 @@ public class Cashier {
         Thread.sleep(2000);
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));  // Ожидание до 10 секунд
+        Actions actions = new Actions(driver); // Вместо клика. Для более точного клика
 
         // Переходим в кассу
         WebElement depositButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li/a[text()='Deposit']")));
@@ -47,6 +49,19 @@ public class Cashier {
         WebElement cashierName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h4[text()='All payment methods']")));
         String cashierNameText = cashierName.getText();
         Assert.assertEquals("All payment methods", cashierNameText);
+
+        WebElement pendingPayoutRequests = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Pending payout requests']")));
+        actions.moveToElement(pendingPayoutRequests).click().perform();
+        WebElement pendingPayoutRequestsTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//th[text()='date of request']")));
+        String pendingPayoutRequestsTitleText = pendingPayoutRequestsTitle.getText();
+        Assert.assertEquals("date of request", pendingPayoutRequestsTitleText);
+
+        WebElement transactionsHistory = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Transactions history']")));
+        actions.moveToElement(transactionsHistory).click().perform();
+        WebElement transactionsHistoryTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form/div/p[text()='From']")));
+        String transactionsHistoryTitleText = transactionsHistoryTitle.getText();
+        Assert.assertEquals("From", transactionsHistoryTitleText);
+
     }
 
     @After
